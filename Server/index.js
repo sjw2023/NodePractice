efa4255840a8c6cbb39d965bf6ed7bee8608493c
JoowonSuh <sjw2023@naver.com>
2019-11-04 17:01:43 -0800

    Installed coockie-session and made app to use cookie with passport

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 
 require("./models/user"); // This should ba called first since it creates model class abd passport is using that class
@@ -8,6 +10,17 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI); //Connect with database with URI of mongoDB Atlas instance
 
 const app = express(); //Creating Wep application
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // I want this cookie last for 30days
+    keys: [keys.cookieKey] //to recognize the cookie we are going to use keys, but we don't want show it to public lets put it into keys.js
+  })
+); //going to tell express that we need to add cookie
+
+//To make passport use cookieKey
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app); //This is using methods in authRoutes.js
 
