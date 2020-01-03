e310b0602a20ef24c482881fd01d697b1c8fe382
JoowonSuh <sjw2023@naver.com>
2020-01-02 20:37:31 -0800

    Refactored passport.use() using async and await. It gave us more readability of the code.

--- services/passport.js
+++ services/passport.js
@@ -27,21 +27,18 @@ passport.use(
       callbackURL: "/auth/google/callback", // error happend 'bad request' -> it needs ful URL
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }) // Check if useris already inside of the collection, It returns the promisses
-        .then(existingUser => {
-          //It returns record if user is exist or null
-          if (existingUser) {
-            //we already have a record with the given profile ID
-            done(null, existingUser); //Something like return, null is return no error, second is record
-          } else {
-            //we don't have a user recod with this ID, make a new record
-            //new User({ googleId: profile.id })//Creating the model instance but not put it into
-            new User({ googleId: profile.id })
-              .save() // this creates the model instance and save into database
-              .then(user => done(null, user)); // if save has been done succesfully return( callback )
-          }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id }); // Check if useris already inside of the collection, It returns the promisses
+      //It returns record if user is exist or null
+      if (existingUser) {
+        //we already have a record with the given profile ID
+        done(null, existingUser); //Something like return, null is return no error, second is record
+      } else {
+        //we don't have a user recod with this ID, make a new record
+        //new User({ googleId: profile.id })//Creating the model instance but not put it into
+        const user = await new User({ googleId: profile.id }).save(); // this creates the model instance and save into database
+        done(null, user); // if save has been done succesfully return( callback )
+      }
       //console.log("access token", accessToken);
       //console.log("regresh token", refreshToken);
       //console.log("profile", profile);
