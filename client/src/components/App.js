f9e70305db486db28d1bc5d986d71215805a16ae
JoowonSuh <sjw2023@naver.com>
2020-01-07 19:37:06 -0800

    Added materialize npm module on client side

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,20 +6,28 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 //Example : Set up a dummy coponent.
 //This dummy component will be functional component which returns h2
-const Header = () => <h2>Header</h2>;
+// const Header = () => <h2>Header</h2>;
+import Header from "./Header"; // remove dummy Header and replace with Header.js
 const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>Surveynew</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
+//BrouwerRouter tag should have at most one child
+//Inside of BrowserRouter, here we are going to put rules of routing
+//path attribute match from the beginning so we should make rule to show exact one.
+//we can solve this by using keyword exact
+//Header will be showed no matter what happend, and we will do this by using Header it self.
+// the Contents of each component will be detemiened by Redux files.
 
 const App = () => {
   return (
     // jaxs
     <div>
       <BrowserRouter>
-        //this should have at most one child
         <div>
-          //here we are going to put rules of routing
-          <Route path="/" component={Landing} />
+          <Header />
+          <Route exact={true} path="/" component={Landing} />
+          <Route exact={true} path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
