25ebb94ddc35871b094fcf7634c4a602e67ab199
JoowonSuh <sjw2023@naver.com>
2020-01-14 16:25:44 -0800

    Added payment component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 //To make detail of header Component
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -14,11 +16,14 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
+        return [
+          <li>
+            <Payments />
+          </li>,
           <li>
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
   render() {
@@ -26,7 +31,13 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo"> Emaily</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            {" "}
+            Emaily{" "}
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
